0ad178bbff98cd3068edd2095aac52234c666483
Yonatan Kassahun <yonsbreak7@gmail.com>
2026-04-15 11:27:48 +0300

    Update jobpositions.php

--- pages/company_structure/jobpositions.php
+++ pages/company_structure/jobpositions.php
@@ -1,5 +1,5 @@
 <!-- JOB POSITIONS -->
 <div class="page" id="p-job-positions">
-  <div class="page-header"><div><div class="page-title">Job Positions</div><div class="page-sub">Define job information</div></div><button class="btn btn-primary" onclick="openJobModal()"><i data-lucide="plus" size="13"></i>New Position</button></div>
+  <div class="page-header"><div><div class="page-title">Job Positions</div><div class="page-sub">Define job information</div></div><button class="btn btn-primary" onclick="openJobModal()"><i data-lucide="plus" size="13"></i>Add new job position</button></div>
   <div id="tbl-job-positions"></div>
 </div>
